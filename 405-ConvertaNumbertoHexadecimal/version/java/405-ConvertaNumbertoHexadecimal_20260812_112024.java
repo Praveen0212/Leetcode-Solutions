04651251027fae2638b60cc969ec0b33792cca38
Praveen0212 <praveenbabuks0212@gmail.com>
2026-08-12 11:20:25 +0530

    Create 405-ConvertaNumbertoHexadecimal

---
+++ 405-ConvertaNumbertoHexadecimal/version/java/405-ConvertaNumbertoHexadecimal_20260812_112024.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 11:20:24 AM
+1class Solution {
+2    public String toHex(int num) {
+3        if(num == 0){
+4            return "0";
+5        }
+6        char hex[] = "0123456789abcdef".toCharArray();
+7        String ans = "";
+8        while(num!=0){
+9            ans = hex[num & 15] + ans;
+10            num = num >>> 4;
+11        }
+12        return ans;
+13    }
+14}
