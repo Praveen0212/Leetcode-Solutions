df7701581b15f851376dc847e68183b0a35c5e8e
Praveen0212 <praveenbabuks0212@gmail.com>
2026-08-12 09:06:38 +0530

    Create 2190-CountCommonWordsWithOneOccurrence

---
+++ 2190-CountCommonWordsWithOneOccurrence/2190-CountCommonWordsWithOneOccurrence.java
@@ -0,0 +1,24 @@
+// Last updated: 8/12/2026, 9:06:38 AM
+class Solution {
+    public int countWords(String[] words1, String[] words2) {
+        int count = 0;
+        for(int i=0;i<words1.length;i++){
+            int c1 = 0;
+            int c2 = 0;
+            for(int j=0;j<words1.length;j++){
+                if(words1[i].equals(words1[j])){
+                    c1++;
+                }
+            }
+            for(int j=0;j<words2.length;j++){
+                if(words1[i].equals(words2[j])){
+                    c2++;
+                }
+            }
+            if(c1==1 && c2==1){
+                count++;
+            }
+        }
+        return count;    
+    }
+}
