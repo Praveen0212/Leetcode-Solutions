120092228d3fe33eb133339918cfe1c165e9f662
Praveen0212 <praveenbabuks0212@gmail.com>
2026-08-12 09:08:13 +0530

    Create 1335-MaximumCandiesAllocatedToKChildren

---
+++ 1335-MaximumCandiesAllocatedToKChildren/1335-MaximumCandiesAllocatedToKChildren.java
@@ -0,0 +1,25 @@
+// Last updated: 8/12/2026, 9:08:13 AM
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int left = 1, right = 10_000_000;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long childrenCount = 0;
+
+            for (int candy : candies) {
+                childrenCount += candy / mid;
+            }
+
+            if (childrenCount >= k) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
